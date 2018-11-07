4e578b23f7a43bbb10187e34e44e7077b0e50239
Raf <rafaellos.g9@gmail.com>
2018-11-07 10:43:42 +0200

    Deleting old project

--- frontend/teamUp_V2/app/src/main/java/com/github/h01d/teamup_v2/network/NetworkManagerListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.h01d.teamup_v2.network;
-
-import org.json.JSONArray;
-
-public interface NetworkManagerListener
-{
-    void getResult(JSONArray result);
-    void getError(String error, int code);
-}
